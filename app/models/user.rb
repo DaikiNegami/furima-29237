2f5a95ed67fb2d053018c82df5173abd3f7b61bb
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-04 15:04:42 +0900

    バリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth_date
+  end
 end
