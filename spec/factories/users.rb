e6f86b3d93aaf4cbf07d36067676262d76372f4d
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-04 15:43:01 +0900

    FactoryBot　定義変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    first_name {Faker::Name.first_name}
-    last_name {Faker::Name.last_name}
+    first_name {"太郎"}
+    last_name {"田中"}
     first_name_kana {"タロウ"}
     last_name_kana {"タナカ"}
     birth_date {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
