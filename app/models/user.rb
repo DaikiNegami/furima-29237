ea47abacaf72702ab8f337a3366831f45fe1c517
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-04 21:02:45 +0900

    正規表現　変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :birth_date
+      validates :nickname
+      PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX, message: "is invalid. Input half-width characters."
+      NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+        validates_format_of :first_name, with: NAME_REGEX, message: 'is invalid. Input full-width characters.' 
+        validates_format_of :last_name, with: NAME_REGEX, message: 'is invalid. Input full-width characters.'
+      KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+        validates_format_of :first_name_kana, with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters.' 
+        validates_format_of :last_name_kana, with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters.'
+      validates :birth_date
   end
 end
