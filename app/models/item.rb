0a8d8c138e115344a76ef2158c42784a4e63b096
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-06 12:10:32 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
-  
+  validates :name, :description, precence: true
+  varidates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :handling_time_id, numericality: { other_than: 1 }
 end
