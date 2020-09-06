ab5779eae2dcf0c590716c9dffa7a2273a522f82
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-06 12:45:34 +0900

    itemsテーブルとのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :handling_time
+  belongs_to_active_hash :shipping_burden
   validates :name, :description, precence: true
   varidates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :handling_time_id, numericality: { other_than: 1 }
 end
