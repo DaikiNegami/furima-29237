3a4496c8c352b93f817eca7e68356ded02a0768e
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-06 14:57:18 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :handling_time
   belongs_to_active_hash :shipping_burden
-  validates :name, :description, precence: true
+  validates :name, :description, :price, precence: true
   varidates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :handling_time_id, numericality: { other_than: 1 }
 end
