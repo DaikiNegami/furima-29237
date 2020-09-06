36364337e2838a0eea7a7f89baf5e3c5f6385311
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-06 22:02:28 +0900

    バリデーション、アソシエーション記述編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,20 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :handling_time
   belongs_to_active_hash :shipping_burden
+  
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
-    validates :name, :description, :image
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :image
     validates :price, numericality: { greater_than: 299, less_than: 10000000 }
   end
-  varidates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :handling_time_id, numericality: { other_than: 1 }
+  validates :category_id, :condition_id, :shipping_burden_id, :prefecture_id, :handling_time_id, numericality: { other_than: 0 }
   
 end
