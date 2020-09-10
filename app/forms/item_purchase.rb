e7f366c1a13fb5f43198547ce466a04b8a5da2a5
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 22:59:41 +0900

    住所　バリデーション移動

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,6 +1,14 @@
 class UserDonation
 
   include ActiveModel::Model
-  attr_accessor 
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input post_code' }
+    validates :city, :address
+    validates :phone_number, format: { with: /\A\d^(0{1}\d{9,10})$\z/, message: 'is invalid. Input phone number within 11 digits' }
+
+  validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+
+  
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -5,10 +5,4 @@ class Address < ApplicationRecord
   belongs_to :purchase
   belongs_to_active_hash :prefecture
 
-  with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input post_code' }
-    validates :city, :address
-    validates :phone_number, format: { with: /\A\d^(0{1}\d{9,10})$\z/, message: 'is invalid. Input phone number within 11 digits' }
-
-  validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
 end
