550f66c4386fb6fbf5ca67288e0e2ff0b405ad55
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 23:37:46 +0900

    token追加

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,11 +1,11 @@
 class UserDonation
 
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input post_code' }
-    validates :city, :address
+    validates :city, :address, :token
     validates :phone_number, format: { with: /\A\d^(0{1}\d{9,10})$\z/, message: 'is invalid. Input phone number within 11 digits' }
 
   validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
