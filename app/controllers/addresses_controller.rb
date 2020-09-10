e594120485c31fd98906d4288baa8f8dad4aa065
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 14:31:22 +0900

    addressesコントローラー作成、編集

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,18 @@
+class AddressesController < ApplicationController
+
+  def create
+    address = Address.create(address_params)
+    if address.save
+      redirect_to root_path
+    else
+      render purchases_path
+    end
+
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(purchase_id: params[:purchase_id])
+  end
+end
