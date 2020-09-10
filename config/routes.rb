321ffce4cd4cc8807368dbee9818f98eb0ee6e5b
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 17:45:25 +0900

    ルーティング記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
   resources :items do
-    resources :purchases, only: [:index, :create] 
-      resources :addresses, only: [:create]
+    resources :purchases, only: [:index, :create]
   end
+  resources :addresses, only: [:create]
+
 end
