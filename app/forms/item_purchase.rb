917ab1909efa3f94fb48a0168316b726a11a19c5
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 23:09:38 +0900

    テーブルへの保存コード記述

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,7 +1,7 @@
 class UserDonation
 
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Input post_code' }
@@ -10,5 +10,9 @@ class UserDonation
 
   validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
 
+  def save
+    Address.create(post_code: post_code, prefecture_id: prefecture.id,city: city, address: address, phone_number: phone_number,purchase_id: purchase.id)
+    Purchase.create(user_id: user.id, item_id: item.id)
+  end
   
 end
