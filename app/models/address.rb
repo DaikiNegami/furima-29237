182b1e326ce74b0eca3d15dced037eaa7d7938db
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 12:31:52 +0900

    アソシエーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,7 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  belongs_to :purchase
+  belongs_to_active_hash :prefecture
 end
