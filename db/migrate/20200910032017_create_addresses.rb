f4c7a4e16c5562f051bb50255f1c25167a29d296
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-10 12:31:06 +0900

    テーブル設計反映

--- db/migrate/20200910032017_create_addresses.rb
+++ db/migrate/20200910032017_create_addresses.rb
@@ -3,10 +3,10 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string :post_code,       null: :false
       t.integer :prefecture_id,  null: :false
-      t.stiring :city,           null: :false
+      t.string :city,           null: :false
       t.string :address,         null: :false
-      t.stiring :building
-      t.stiring :phone_number,   null: :false
+      t.string :building
+      t.string :phone_number,   null: :false
       t.references :purchase,    null: false, foreign_key: true
       t.timestamps
     end
