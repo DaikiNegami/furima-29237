82399db056fabc9df7a23af0999d4b851ae86843
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-11 18:50:13 +0900

    購入に関するコントローラー編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,29 +2,32 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase = ItemPurchase.new
   end
 
   def create
-    @purchase = ItemPurchase.new(user_id: purchase_params[:user_id], item_id: puchase_params[:item_id])
+    @item = Item.find(params[:item_id])
+    @purchase = ItemPurchase.new(purchase_params)
     if @purchase.valid?
       pay_item
       @purchase.save
       return redirect_to root_path
     else
-      render item_purchases_path(@item)
+      render :index
     end
   end
   
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:token).merge(user_id: current_user.id, purchase_id: params[:purchase_id])
+    params.require(:item_purchase).permit(:token, :post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
     Payjp.api_key = "sk_test_4c4f6d2a57b05977da374a77"
     Payjp::Charge.create(
-      card: order_params[:token],
+      amount: @item.price,
+      card: purchase_params[:token],
       currency:'jpy'
     )
   end
