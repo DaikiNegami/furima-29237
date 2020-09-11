a7323e9db71f0a0296dbb6ea719a3f770e300593
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-11 18:49:40 +0900

    余計なコントローラー記述削除

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,1 @@
-class AddressesController < ApplicationController
 
-  def create
-    @address = ItemPurchase.new(address_params)
-    if @address.save
-      redirect_to root_path
-    else
-      render item_purchases_path(@item)
-    end
-
-  end
-
-  private
-
-  def address_params
-    params.require(:item_purchase).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(purchase_id: params[:purchase_id])
-  end
-end
