3b3d281c65386d7eefee053facc3225d4ef2a348
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-14 16:28:44 +0900

    factorybot編集

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase do
-    
+    token {"sampletoken"}
+    post_code {123-4567}
+    prefecture_id {1}
+    city {"横浜市緑区"}
+    address {"青山1-1-1"}
+    building {}
+    phone_number {Faker::Number.number(digits: 11)}
   end
 end
