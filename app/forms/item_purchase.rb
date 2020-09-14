c3699ec8e485509ee436a8a38d3dc6fc744410e3
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-14 15:13:54 +0900

    formオブジェクト内記述変更

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -12,8 +12,8 @@ class ItemPurchase
   validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
 
   def save
-    Address.create(post_code: post_code, prefecture_id: prefecture.id,city: city, address: address, phone_number: phone_number,purchase_id: purchase_id)
-    Purchase.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, purchase_id: purchase.id)
   end
   
 end
