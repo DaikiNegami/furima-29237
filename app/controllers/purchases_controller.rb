bf4e2f60b6fb2a840ae8a4d2169158ff001f39bd
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-14 22:49:54 +0900

    before_action　定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase = ItemPurchase.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase = ItemPurchase.new(purchase_params)
     if @purchase.valid?
       pay_item
@@ -18,6 +17,10 @@ class PurchasesController < ApplicationController
   end
   
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:item_purchase).permit(:token, :post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
