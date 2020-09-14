c1f060c494f6836fb171f0def0a09a853151d41c
DaikiNegami <d_0615.baseball@icloud.com>
2020-09-14 17:14:56 +0900

    factorybot post_code 編集

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase do
     token {"sampletoken"}
-    post_code {123-4567}
+    post_code {"123-4567"}
     prefecture_id {1}
     city {"横浜市緑区"}
     address {"青山1-1-1"}
